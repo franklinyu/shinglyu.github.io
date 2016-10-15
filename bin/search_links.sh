d8215aa876180b95592f176622ce43a513dabab5
Shing Lyu <shing.lyu@gmail.com>
2016-10-15 18:48:41 +0800

    Search images

--- bin/search_links.sh
+++ bin/search_links.sh
@@ -1,5 +1,15 @@
 #/usr/bin/env bash
+echo "Links"
+echo "-----------------"
 grep -oP "\[[^\]]*\]()" "$1" |\
   sed -E "s/\[|\]|\`//g" |\
   sed -E "s/ /+/g" |\
   sed "s/^/https:\/\/www.google.com\/search\?q\=/g"
+
+echo ""
+echo "Images"
+echo "-----------------"
+grep -oP "!\[[^\]]*\]()" "$1" |\
+  sed -E "s/!|\[|\]|\`//g" |\
+  sed -E "s/ /+/g" |\
+  sed "s/^/https:\/\/www.google.com\/images\?q\=/g"
