f32d68422e05f765c70068c6b46a9916744d6f1a
Shing Lyu <slyu@mozilla.com>
2017-07-11 15:55:19 +0800

    Updated search path script

--- bin/search_links.sh
+++ bin/search_links.sh
@@ -1,18 +1,31 @@
 #/usr/bin/env bash
 
+# Usage:
+# Write your link in the following format:
+# [Mozilla Offical Site][mozofficial]
+# // [mozofficial]: https://www.mozilla.org
+
 get_links() {
   grep -oP "\[[^\]]*\]\[[^\]]*\]" "${1}" |\
     cut -d "[" -f 3 |\
     sed "s/]$//g" 
 }
+
+get_links_text() {
+  grep -oP "\[[^\]]*\]\[[^\]]*\]" "${1}" |\
+    cut -d "[" -f 2 |\
+    sed "s/]$//g"
+}
+
 echo "Links"
 echo "-----------------"
 
-get_links "${1}" |\
+get_links_text "${1}" |\
   sed -E "s/ /+/g" |\
   sed "s/^/https:\/\/www.google.com\/search\?q\=/g"
 
 echo "-----------------"
+
 get_links "${1}" |\
   sed "s/^/[/g" |\
   sed "s/$/]: /g"
